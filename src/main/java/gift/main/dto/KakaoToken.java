6e9748105ce7ecf70cb913adef52b0a2e4c7e4f3
JinSeoHyun <15roa1121@gmail.com>
2024-08-05 23:08:49 +0900

    refactor: 필드들 카멜 케이스 규칙 적용

--- src/main/java/gift/main/dto/KakaoToken.java
+++ src/main/java/gift/main/dto/KakaoToken.java
@@ -7,14 +7,14 @@ import java.time.LocalDateTime;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoToken(
-        String accessToken,                 //사용자 액세스 토큰 값
-        String tokenType,                   //토큰타입, bearer로 고정
-        String refreshToken,                //사용자 리프레시 토큰 값
-        int expiresIn,                     //엑세스 토큰 시간 (초)
-        int refreshTokenExpiresIn,        //리프레시 토큰 시간
+        String accessToken,                             //사용자 액세스 토큰 값
+        String tokenType,                               //토큰타입, bearer로 고정
+        String refreshToken,                            //사용자 리프레시 토큰 값
+        int expiresIn,                                  //엑세스 토큰 시간 (초)
+        int refreshTokenExpiresIn,                      //리프레시 토큰 시간
         LocalDateTime accessTokenExpirationDate         //직접 계산한 만료시간
 ) {
     public KakaoToken(TempToken token, LocalDateTime createDateTime) {
-        this(token.accessToken(), token.tokenType(), token.refreshToken(), token.expires_in(), token.refresh_token_expires_in(), createDateTime.plusSeconds(token.expires_in()));
+        this(token.accessToken(), token.tokenType(), token.refreshToken(), token.expiresIn(), token.refreshTokenExpiresIn(), createDateTime.plusSeconds(token.expiresIn()));
     }
 }
--- src/main/java/gift/main/dto/TempToken.java
+++ src/main/java/gift/main/dto/TempToken.java
@@ -8,8 +8,8 @@ public record TempToken(
         String accessToken,                 //사용자 액세스 토큰 값
         String tokenType,                   //토큰타입, bearer로 고정
         String refreshToken,                //사용자 리프레시 토큰 값
-        int expires_in,                     //엑세스 토큰 시간
-        int refresh_token_expires_in        //리프레시 토큰 시간
+        int expiresIn,                     //엑세스 토큰 시간
+        int refreshTokenExpiresIn        //리프레시 토큰 시간
 ) {
 
 }
